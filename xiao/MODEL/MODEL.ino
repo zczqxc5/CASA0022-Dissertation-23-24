26e5ab05be0da3dc09bd926646a233c75bb26a90
zczqxc5 <146037962+zczqxc5@users.noreply.github.com>
2024-07-08 18:41:29 +0100

    bluetooth

--- xiao/MODEL/MODEL.ino
+++ xiao/MODEL/MODEL.ino
@@ -1,24 +1,38 @@
 #include <fan_inferencing.h>
 #include <Wire.h>
 #include <LSM6DS3.h>
+#include <st7789v2.h>
+#include "SPI.h"
+#include "seeed.h"
+#include <bluefruit.h>
+
+#define SCREEN_WIDTH 240
+#define SCREEN_HEIGHT 280
+int getStringWidth(const char *str, const sFONT *font) {
+  return strlen(str) * font->Width;
+}
+
+// 显示屏初始化
+st7789v2 Display;
+
 
 // 初始化传感器对象
 LSM6DS3 myIMU(I2C_MODE, 0x6A);  // 默认I2C地址0x6A
 
 enum sensor_status {
-    NOT_USED = -1,
-    NOT_INIT,
-    INIT,
-    SAMPLED
+  NOT_USED = -1,
+  NOT_INIT,
+  INIT,
+  SAMPLED
 };
 
 /** Struct to link sensor axis name to sensor value function */
-typedef struct{
-    const char *name;
-    float *value;
-    uint8_t (*poll_sensor)(void);
-    bool (*init_sensor)(void);    
-    sensor_status status;
+typedef struct {
+  const char *name;
+  float *value;
+  uint8_t (*poll_sensor)(void);
+  bool (*init_sensor)(void);
+  sensor_status status;
 } eiSensors;
 
 /* Constant defines -------------------------------------------------------- */
@@ -32,203 +46,287 @@ int8_t fusion_sensors[N_SENSORS];
 int fusion_ix = 0;
 
 bool init_IMU(void) {
-    static bool init_status = false;
-    if (!init_status) {
-        init_status = (myIMU.begin() == 0);
-        if (init_status) {
-            Serial.println("IMU initialized successfully");
-        } else {
-            Serial.println("IMU initialization failed");
-        }
+  static bool init_status = false;
+  if (!init_status) {
+    init_status = (myIMU.begin() == 0);
+    if (init_status) {
+      Serial.println("IMU initialized successfully");
+    } else {
+      Serial.println("IMU initialization failed");
     }
-    return init_status;
+  }
+  return init_status;
 }
 
 uint8_t poll_acc(void) {
-    data[0] = myIMU.readFloatAccelX();
-    data[1] = myIMU.readFloatAccelY();
-    data[2] = myIMU.readFloatAccelZ();
+  data[0] = myIMU.readFloatAccelX();
+  data[1] = myIMU.readFloatAccelY();
+  data[2] = myIMU.readFloatAccelZ();
 
-    for (int i = 0; i < 3; i++) {
-        if (fabs(data[i]) > MAX_ACCEPTED_RANGE) {
-            data[i] = (data[i] > 0 ? 1 : -1) * MAX_ACCEPTED_RANGE;
-        }
+  for (int i = 0; i < 3; i++) {
+    if (fabs(data[i]) > MAX_ACCEPTED_RANGE) {
+      data[i] = (data[i] > 0 ? 1 : -1) * MAX_ACCEPTED_RANGE;
     }
+  }
 
-    data[0] *= CONVERT_G_TO_MS2;
-    data[1] *= CONVERT_G_TO_MS2;
-    data[2] *= CONVERT_G_TO_MS2;
+  data[0] *= CONVERT_G_TO_MS2;
+  data[1] *= CONVERT_G_TO_MS2;
+  data[2] *= CONVERT_G_TO_MS2;
 
-    return 0;
+  return 0;
 }
 
 uint8_t poll_gyr(void) {
-    data[3] = myIMU.readFloatGyroX();
-    data[4] = myIMU.readFloatGyroY();
-    data[5] = myIMU.readFloatGyroZ();
+  data[3] = myIMU.readFloatGyroX();
+  data[4] = myIMU.readFloatGyroY();
+  data[5] = myIMU.readFloatGyroZ();
 
-    return 0;
+  return 0;
 }
 
 /** Used sensors value function connected to label name */
-eiSensors sensors[] =
-{
-    {"accX", &data[0], &poll_acc, &init_IMU, NOT_USED},
-    {"accY", &data[1], &poll_acc, &init_IMU, NOT_USED},
-    {"accZ", &data[2], &poll_acc, &init_IMU, NOT_USED},
-    {"gyrX", &data[3], &poll_gyr, &init_IMU, NOT_USED},
-    {"gyrY", &data[4], &poll_gyr, &init_IMU, NOT_USED},
-    {"gyrZ", &data[5], &poll_gyr, &init_IMU, NOT_USED},
+eiSensors sensors[] = {
+  { "accX", &data[0], &poll_acc, &init_IMU, NOT_USED },
+  { "accY", &data[1], &poll_acc, &init_IMU, NOT_USED },
+  { "accZ", &data[2], &poll_acc, &init_IMU, NOT_USED },
+  { "gyrX", &data[3], &poll_gyr, &init_IMU, NOT_USED },
+  { "gyrY", &data[4], &poll_gyr, &init_IMU, NOT_USED },
+  { "gyrZ", &data[5], &poll_gyr, &init_IMU, NOT_USED },
 };
 
+// I2C地址，根据您的设备更改此地址
+#define MOTOR_I2C_ADDRESS 0x10
+
+BLEUart bleuart;
+
 void setup() {
-    Serial.begin(115200);
-    while (!Serial);
+  Bluefruit.begin();
+  Bluefruit.Periph.setConnectCallback(connect_callback);
+  Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
+  Bluefruit.setName("Xiao nRF52840");  // 设置设备名称
+  // 开始广告
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+  Bluefruit.Advertising.addName();
+  Bluefruit.Advertising.addService(bleuart);
+  Bluefruit.ScanResponse.addName();
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 244); // 设置广告间隔
+  Bluefruit.Advertising.setFastTimeout(30);   // 设置快速广告超时
+  Bluefruit.Advertising.start(0);             // 开始广告
 
-    Serial.println("Starting initialization...");
 
-    Serial.print("Required sensors: ");
-    Serial.println(EI_CLASSIFIER_FUSION_AXES_STRING);
+  Serial.begin(115200);
 
-    if (!ei_connect_fusion_list(EI_CLASSIFIER_FUSION_AXES_STRING)) {
-        Serial.println("ERR: Errors in sensor list detected");
-        return;
+
+  // 初始化显示屏
+  Display.SetRotate(180);
+  Display.Init();
+  Display.SetBacklight(100);
+  Display.Clear(WHITE);
+
+  // 初始化I2C
+  Wire.begin();
+
+  Serial.println("Starting initialization...");
+
+  Serial.print("Required sensors: ");
+  Serial.println(EI_CLASSIFIER_FUSION_AXES_STRING);
+
+  if (!ei_connect_fusion_list(EI_CLASSIFIER_FUSION_AXES_STRING)) {
+    Serial.println("ERR: Errors in sensor list detected");
+    return;
+  }
+
+  for (int i = 0; i < fusion_ix; i++) {
+    if (sensors[fusion_sensors[i]].status == NOT_INIT) {
+      sensors[fusion_sensors[i]].status = (sensor_status)sensors[fusion_sensors[i]].init_sensor();
+      if (!sensors[fusion_sensors[i]].status) {
+        Serial.print(sensors[fusion_sensors[i]].name);
+        Serial.println(" axis sensor initialization failed.");
+      } else {
+        Serial.print(sensors[fusion_sensors[i]].name);
+        Serial.println(" axis sensor initialization successful.");
+      }
     }
-
-    for (int i = 0; i < fusion_ix; i++) {
-        if (sensors[fusion_sensors[i]].status == NOT_INIT) {
-            sensors[fusion_sensors[i]].status = (sensor_status)sensors[fusion_sensors[i]].init_sensor();
-            if (!sensors[fusion_sensors[i]].status) {
-                Serial.print(sensors[fusion_sensors[i]].name);
-                Serial.println(" axis sensor initialization failed.");
-            } else {
-                Serial.print(sensors[fusion_sensors[i]].name);
-                Serial.println(" axis sensor initialization successful.");
-            }
-        }
-    }
+  }
 }
 
 void loop() {
-    Serial.println("\nStarting inferencing in 2 seconds...");
-    delay(2000);
+  Serial.println("\nStarting inferencing in 2 seconds...");
+  delay(2000);
 
-    if (EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME != fusion_ix) {
-        Serial.print("ERR: Sensors don't match the sensors required in the model\n");
-        Serial.print("Following sensors are required: ");
-        Serial.println(EI_CLASSIFIER_FUSION_AXES_STRING);
-        return;
+  if (EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME != fusion_ix) {
+    Serial.print("ERR: Sensors don't match the sensors required in the model\n");
+    Serial.print("Following sensors are required: ");
+    Serial.println(EI_CLASSIFIER_FUSION_AXES_STRING);
+    return;
+  }
+
+  Serial.println("Sampling...");
+
+  float buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE] = { 0 };
+
+  for (size_t ix = 0; ix < EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE; ix += EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME) {
+    int64_t next_tick = (int64_t)micros() + ((int64_t)EI_CLASSIFIER_INTERVAL_MS * 1000);
+
+    for (int i = 0; i < fusion_ix; i++) {
+      if (sensors[fusion_sensors[i]].status == INIT) {
+        sensors[fusion_sensors[i]].poll_sensor();
+        sensors[fusion_sensors[i]].status = SAMPLED;
+      }
+      if (sensors[fusion_sensors[i]].status == SAMPLED) {
+        buffer[ix + i] = *sensors[fusion_sensors[i]].value;
+        sensors[fusion_sensors[i]].status = INIT;
+      }
     }
 
-    Serial.println("Sampling...");
-
-    float buffer[EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE] = { 0 };
-
-    for (size_t ix = 0; ix < EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE; ix += EI_CLASSIFIER_RAW_SAMPLES_PER_FRAME) {
-        int64_t next_tick = (int64_t)micros() + ((int64_t)EI_CLASSIFIER_INTERVAL_MS * 1000);
-
-        for (int i = 0; i < fusion_ix; i++) {
-            if (sensors[fusion_sensors[i]].status == INIT) {
-                sensors[fusion_sensors[i]].poll_sensor();
-                sensors[fusion_sensors[i]].status = SAMPLED;
-            }
-            if (sensors[fusion_sensors[i]].status == SAMPLED) {
-                buffer[ix + i] = *sensors[fusion_sensors[i]].value;
-                sensors[fusion_sensors[i]].status = INIT;
-            }
-        }
-
-        int64_t wait_time = next_tick - (int64_t)micros();
-        if (wait_time > 0) {
-            delayMicroseconds(wait_time);
-        }
+    int64_t wait_time = next_tick - (int64_t)micros();
+    if (wait_time > 0) {
+      delayMicroseconds(wait_time);
     }
+  }
 
-    signal_t signal;
-    int err = numpy::signal_from_buffer(buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
-    if (err != 0) {
-        Serial.print("ERR: ");
-        Serial.println(err);
-        return;
-    }
+  signal_t signal;
+  int err = numpy::signal_from_buffer(buffer, EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, &signal);
+  if (err != 0) {
+    Serial.print("ERR: ");
+    Serial.println(err);
+    return;
+  }
 
-    ei_impulse_result_t result = { 0 };
-    err = run_classifier(&signal, &result, false);
-    if (err != EI_IMPULSE_OK) {
-        Serial.print("ERR: ");
-        Serial.println(err);
-        return;
-    }
+  ei_impulse_result_t result = { 0 };
+  err = run_classifier(&signal, &result, false);
+  if (err != EI_IMPULSE_OK) {
+    Serial.print("ERR: ");
+    Serial.println(err);
+    return;
+  }
 
-    Serial.print("Predictions (DSP: ");
-    Serial.print(result.timing.dsp);
-    Serial.print(" ms., Classification: ");
-    Serial.print(result.timing.classification);
-    Serial.print(" ms., Anomaly: ");
-    Serial.print(result.timing.anomaly);
-    Serial.println(" ms.):");
+  Serial.print("Predictions (DSP: ");
+  Serial.print(result.timing.dsp);
+  Serial.print(" ms., Classification: ");
+  Serial.print(result.timing.classification);
+  Serial.print(" ms., Anomaly: ");
+  Serial.print(result.timing.anomaly);
+  Serial.println(" ms.):");
+  display_max_probability_label(result);
 
-    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
-        Serial.print(result.classification[ix].label);
-        Serial.print(": ");
-        Serial.println(result.classification[ix].value);
-    }
-    #if EI_CLASSIFIER_HAS_ANOMALY == 1
-    Serial.print("    anomaly score: ");
-    Serial.println(result.anomaly);
-    #endif
+  for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+    Serial.print(result.classification[ix].label);
+    Serial.print(": ");
+    Serial.println(result.classification[ix].value);
+
+
+
+    char value_str[10];
+    dtostrf(result.classification[ix].value, 6, 2, value_str);
+  }
+#if EI_CLASSIFIER_HAS_ANOMALY == 1
+  Serial.print("    anomaly score: ");
+  Serial.println(result.anomaly);
+
+
+  char anomaly_str[10];
+  dtostrf(result.anomaly, 6, 2, anomaly_str);
+
+#endif
+
+  delay(1000);  // 主循环延时
 }
 
 static int8_t ei_find_axis(char *axis_name) {
-    for (int ix = 0; ix < N_SENSORS; ix++) {
-        if (strstr(axis_name, sensors[ix].name)) {
-            return ix;
-        }
+  for (int ix = 0; ix < N_SENSORS; ix++) {
+    if (strstr(axis_name, sensors[ix].name)) {
+      return ix;
     }
-    return -1;
+  }
+  return -1;
 }
 
 
 static bool ei_connect_fusion_list(const char *input_list) {
-    char *buff;
-    bool is_fusion = false;
+  char *buff;
+  bool is_fusion = false;
 
-    /* Copy const string in heap mem */
-    char *input_string = (char *)ei_malloc(strlen(input_list) + 1);
-    if (input_string == NULL) {
-        Serial.println("Error allocating memory for input_string");
-        return false;
-    }
-    memset(input_string, 0, strlen(input_list) + 1);
-    strncpy(input_string, input_list, strlen(input_list));
+  /* Copy const string in heap mem */
+  char *input_string = (char *)ei_malloc(strlen(input_list) + 1);
+  if (input_string == NULL) {
+    Serial.println("Error allocating memory for input_string");
+    return false;
+  }
+  memset(input_string, 0, strlen(input_list) + 1);
+  strncpy(input_string, input_list, strlen(input_list));
 
-    /* Clear fusion sensor list */
-    memset(fusion_sensors, 0, N_SENSORS);
-    fusion_ix = 0;
+  /* Clear fusion sensor list */
+  memset(fusion_sensors, 0, N_SENSORS);
+  fusion_ix = 0;
 
-    buff = strtok(input_string, "+");
-    while (buff != NULL) { /* Run through buffer */
-        int8_t found_axis = ei_find_axis(buff);
-        Serial.print("Checking axis: ");
-        Serial.println(buff);
+  buff = strtok(input_string, "+");
+  while (buff != NULL) { /* Run through buffer */
+    int8_t found_axis = ei_find_axis(buff);
+    Serial.print("Checking axis: ");
+    Serial.println(buff);
 
-        if (found_axis >= 0) {
-            if (fusion_ix < N_SENSORS) {
-                fusion_sensors[fusion_ix++] = found_axis;
-                sensors[found_axis].status = NOT_INIT;
-            }
-            is_fusion = true;
-            Serial.print("Found axis: ");
-            Serial.println(sensors[found_axis].name);
-        } else {
-            Serial.print("Axis not found: ");
-            Serial.println(buff);
-        }
-
-        buff = strtok(NULL, "+ ");
+    if (found_axis >= 0) {
+      if (fusion_ix < N_SENSORS) {
+        fusion_sensors[fusion_ix++] = found_axis;
+        sensors[found_axis].status = NOT_INIT;
+      }
+      is_fusion = true;
+      Serial.print("Found axis: ");
+      Serial.println(sensors[found_axis].name);
+    } else {
+      Serial.print("Axis not found: ");
+      Serial.println(buff);
     }
 
-    ei_free(input_string);
+    buff = strtok(NULL, "+ ");
+  }
 
-    return is_fusion;
+  ei_free(input_string);
+
+  return is_fusion;
+}
+
+void display_max_probability_label(ei_impulse_result_t result) {
+  float max_value = 0.0;
+  const char *max_label = nullptr;
+
+  // 找出最大可能性的标签
+  for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+    if (result.classification[ix].value > max_value) {
+      max_value = result.classification[ix].value;
+      max_label = result.classification[ix].label;
+    }
+  }
+
+  // 打印和显示最大可能性的标签
+  if (max_label != nullptr) {
+    Serial.print("Max label: ");
+    Serial.println(max_label);
+
+    Display.Clear(WHITE);  // 清除屏幕，每次显示新结果前清空屏幕
+    int text_width = getStringWidth(max_label, &Font20);
+    int x_center = (SCREEN_WIDTH - text_width) / 2;                               // 计算居中位置的X坐标
+    int y_center = (SCREEN_HEIGHT - Font20.Height) / 2;                           // 计算居中位置的Y坐标
+    Display.DrawString_EN(x_center, y_center, max_label, &Font40, WHITE, BLACK);  // 显示最大可能性的标签
+    // 通过蓝牙发送 max_label
+    if (bleuart.connected()) {
+      bleuart.print("Max label: ");
+      bleuart.println(max_label);
+
+  }
+}
+
+// 蓝牙连接回调函数
+void connect_callback(uint16_t conn_handle) {
+  // 蓝牙连接成功时的回调函数
+  Serial.println("Connected!");
+}
+
+// 蓝牙断开连接回调函数
+void disconnect_callback(uint16_t conn_handle, uint8_t reason) {
+  // 蓝牙断开连接时的回调函数
+  Serial.println("Disconnected!");
 }
